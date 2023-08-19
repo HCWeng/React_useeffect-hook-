d9267dc5d7ee3183e604e9bd8ce0f23e2ddeb2c3
ArzbondSF <arzbond@gmail.com>
2023-08-19 11:03:48 -0500

    update 19

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,6 +7,9 @@ const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <button onClick={props.onLogout} className="logout-button">
+        Logout
+      </button>
     </Card>
   );
 };
---
+++ src/components/store/auth-context.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AuthContext = React.createContext({ isLoggedId: false });
+
+export default AuthContext;
